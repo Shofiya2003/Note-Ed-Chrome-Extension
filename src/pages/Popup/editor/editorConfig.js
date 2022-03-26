a06894cb458e5fedd4402e220c29a60fc44be9fc
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2022-03-26 20:14:31 +0530

    Update : Editor colors

--- src/pages/Popup/editor/editorConfig.js
+++ src/pages/Popup/editor/editorConfig.js
@@ -1,16 +1,15 @@
 import tools from "./commonTools"
 
-const  config  =  {
-        holder : 'editorjs',
-          autofocus: true,
-          readOnly:false,
-          placeholder: 'Let`s write an awesome story!',
-          tools: tools,
-          data : {
-            time: 1556098174501,
-            blocks: [
-            ],
-            version: "2.12.4"
-          }
+const config = {
+  holder: 'editorjs',
+  autofocus: true,
+  readOnly: false,
+  tools: tools,
+  data: {
+    time: 1556098174501,
+    blocks: [
+    ],
+    version: "2.12.4"
+  }
 }
 export default config
