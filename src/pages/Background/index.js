39bba5f5b3ecab09c55826adf794c0418c610f7a
Ishan <56102033+ishanjain1502@users.noreply.github.com>
2022-03-26 08:53:17 +0530

    got data from yt page

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,16 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+
+// function logTabs(tabs) {
+   
+//     console.log(tabs[0].url);
+//   }
+  
+//   function onError(error) {
+//     console.log(`Error: ${error}`);
+//   }
+  
+//   let querying = chrome.tabs.query({currentWindow: true, active: true});
+//   querying.then(logTabs, onError);
+
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/img/logo.svg';
 import './Popup.css';
 
 const Popup = () => {
+const [ctab, useCtab] = useState("");
+const [count, setCount] = useState(0);
+const [currentTS , setCurrentTS] = useState(null);
+const [title, setTitle] = useState("");
+
+  function logTabs(tabs) {
+    useCtab(ctab => tabs[0].url);
+    console.log(ctab);
+  }
+  
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+  
+  let querying = chrome.tabs.query({currentWindow: true, active: true});
+  querying.then(logTabs, onError);
+
+function recorder(e){
+  e.preventDefault()
+
+  let something = document.getElementsByClassName("ytp-progress-bar");
+  let ts = something
+  console.log(...ts);
+  let name = document.getElementsByClassName("ytp-title-link yt-uix-sessionlink ytp-title-fullerscreen-link")
+  console.log(name);
+}
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +37,8 @@ const Popup = () => {
         <p>
           Welcome to Note-Ed
         </p>
+        
+    <button onClick={recorder}  >Record event</button>
       </header>
     </div>
   );
