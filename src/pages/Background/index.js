bffe53a5e6f1552a4aee2e28d1f183a4302d86ce
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2022-03-26 18:02:35 +0530

    Update : replace browser with chrome

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,22 +1,22 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
 
-chrome.action.onClicked.addListener((tab)=>{
-    console.log("im");
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['content-script.js']
-      });
+chrome.action.onClicked.addListener((tab) => {
+  console.log("im");
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['content-script.js']
+  });
 })
 
 var css = "body { background-color: coral; }";
 
-browser.browserAction.onClicked.addListener(() => {
+chrome.browserAction.onClicked.addListener(() => {
 
   function onError(error) {
     console.log(`Error: ${error}`);
   }
 
-  var insertingCSS = browser.tabs.insertCSS({code: css});
+  var insertingCSS = chrome.tabs.insertCSS({ code: css });
   insertingCSS.then(null, onError);
 });
