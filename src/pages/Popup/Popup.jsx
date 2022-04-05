f6d2fa56d2bee6b923bb3c8bbb74abb955e47ed2
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2022-04-05 20:00:11 +0530

    Add : HomeFrame component to render editor or video home

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,25 @@
 import React, { useState } from 'react';
-
 import './Popup.css';
-import Videohome from "./video/Videohome"
-import Editor from './editor/Editor';
 import PopupNav from './popupNav/PopupNav';
 import Login from './login/Login';
-
+import HomeFrame from './HomeFrame';
 
 const Popup = () => {
-
     const [isLoggedin, setisLoggedin] = useState(false);
+    const [editorActive, seteditorActive] = useState(false);
+
     chrome.storage.sync.get(['loggedInStatus'], function (result) {
         console.log('loggedInStatus is ' + result.loggedInStatus);
         if (result.loggedInStatus) {
             setisLoggedin(true);
         }
     });
-    const [editorActive, seteditorActive] = useState(false);
+
     return (
         <div className="App">
             <PopupNav />
-            {isLoggedin ? <Videohome seteditorActive={seteditorActive} /> : <Login setisLoggedin={setisLoggedin} />}
-            {/* {editorActive ? <Editor seteditorActive={seteditorActive} /> : <Videohome seteditorActive={seteditorActive} />} */}
+            {isLoggedin ? <HomeFrame seteditorActive={seteditorActive} editorActive={editorActive} /> : <Login setisLoggedin={setisLoggedin} />}
+
             {/* <Editor /> */}
             {/* <Videohome seteditorActive={seteditorActive} /> */}
 
