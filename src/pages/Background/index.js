8c266d3202e7006da087cf4c5c52ce192e2794e3
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2022-04-05 21:39:27 +0530

    Done with Auth in front-end with dummmy dashboard

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -2,11 +2,11 @@ chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => 
     if (request.authInfo) {
         console.log(JSON.parse(request.authInfo));
         // localStorage.setItem('jwt', request.authInfo);
-        sendResponse({ success: true, message: 'Auth Info has been received' });
+        sendResponse({ success: true, message: 'Auth Info has been received by background page' });
     } else {
         console.log(request)
         console.log("Auth info not received!");
-        sendResponse({ success: false, message: 'Auth info not received!' });
+        sendResponse({ success: false, message: 'Auth info not received by background page!' });
     }
 });
 
