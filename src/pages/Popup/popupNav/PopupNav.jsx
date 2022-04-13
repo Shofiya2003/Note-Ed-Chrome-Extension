2cb2f7ed221caa0bc528ef3979900af9772c0c0f
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2022-04-13 10:07:59 +0530

    Remove : Devtools by extension

--- src/pages/Popup/popupNav/PopupNav.jsx
+++ src/pages/Popup/popupNav/PopupNav.jsx
@@ -7,10 +7,10 @@ import settingIcon from "../../../assets/img/settings-32.svg"
 export default function PopupNav() {
     return (
         <div className='popup-nav'>
-            <div className="hero">Sasta Notion</div>
+            <div className="hero" title='Sasta Notion'>Sasta Notion</div>
             <div className="nav">
                 <a className='nav-icon' href="#gf" rel="noreferrer"><img src={deleteIcon} className="action-btn-icon" alt="clear notes icon" title='Clear Notes' /></a>
-                <a className='nav-icon' href="#gf"><img src={settingIcon} className="action-btn-icon" alt="settings" title='settings' /></a>
+                <a className='nav-icon' href="chrome-extension://fkldjphfipjbgmadnppjeebikbhoaelm/options.html"><img src={settingIcon} className="action-btn-icon" alt="settings" title='settings' /></a>
                 <a className='nav-icon' href="https://linktr.ee/prathameshdukare" target="_blank" rel="noreferrer"><img src={redirectIcon} className="action-btn-icon" alt="redirect icon" title='Go to Dashboard' /></a>
             </div>
 
