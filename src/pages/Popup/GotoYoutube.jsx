6835e0382c1c90889cad418882d35ac8aa04b368
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2022-04-22 17:36:44 +0530

    started refactor

--- src/pages/Popup/GotoYoutube.jsx
+++ src/pages/Popup/GotoYoutube.jsx
@@ -6,7 +6,7 @@ export default function GotoYoutube() {
     return (
         <div className='goto-yt'>
 
-            <h1 className='hero-name bg-indigo-300'>Sasta Notion</h1>
+            <h1 className='hero-name'>Noted</h1>
             <div className="info">
                 <img src={youtubeLogo} alt="youtube" />
                 <h2>Currently we are only supporting note taking on Youtube</h2>
--- src/pages/Popup/popupNav/PopupNav.jsx
+++ src/pages/Popup/popupNav/PopupNav.jsx
@@ -7,7 +7,7 @@ import settingIcon from "../../../assets/img/settings-32.svg"
 export default function PopupNav() {
     return (
         <div className='popup-nav'>
-            <div className="hero" title='Sasta Notion'>Sasta Notion</div>
+            <div className="hero" title='Sasta Notion'>Noted</div>
             <div className="nav">
                 <a className='nav-icon' href="#gf" rel="noreferrer"><img src={deleteIcon} className="action-btn-icon" alt="clear notes icon" title='Clear Notes' /></a>
                 <a className='nav-icon' href="chrome-extension://fkldjphfipjbgmadnppjeebikbhoaelm/options.html"><img src={settingIcon} className="action-btn-icon" alt="settings" title='settings' /></a>
